6c93a4cbcf8c9314f223dc5f7db09f4594328f82
Antonio Molinari <antoniomolinari@me.com>
2026-04-28 19:09:04 +0200

    Update item name display in list component to capitalize first letters

--- src/components/list.vue
+++ src/components/list.vue
@@ -31,7 +31,7 @@ onMounted(() => {
   <div class="text-center">
     <h1 class="text-4xl font-bold mb-4">List of Items</h1>
     <div v-for="item in items" :key="item.id" class="flex flex-col gap-1 max-w-md mx-auto mb-4">
-      <h2 class="text-2xl font-bold">{{ item.name }}</h2>
+      <h2 class="text-2xl font-bold capitalize">{{ item.name }}</h2>
       <p class="text-gray-500">{{ item.description }}</p>
     </div>
   </div>
